34e0d04c53348bf36d75e5e0b41c56f95aba31b3
Gabriel C3n <ur.gabriel@outlook.com>
2020-07-23 23:40:18 +0200

    Initial commit

---
+++ recommendations.py
@@ -0,0 +1,84 @@
+# c3nsored 
+from math import sqrt
+
+# A dictionary of movie critics and their ratings of a small
+# set of movies
+
+
+
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 3.5},
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+'The Night Listener': 4.5, 'Superman Returns': 4.0,
+'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+'You, Me and Dupree': 2.0},
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+
+# Returns a distance based similarity score for x and y
+# Score is 0-1 1 being the highest
+def sim_distance(prefs, x, y):
+    # Get the list of shared_items
+    si = {}
+    for item in prefs[x]:
+        if item in prefs[y]:
+            si[item] = 1
+
+    # if no ratings are common then return 0
+    if len(si) == 0: return 0
+
+    # Add up the squares of all the differences 
+    sum_of_squares = sum([pow(prefs[x][item]-prefs[y][item], 2) 
+                        for item in prefs[x] if item in prefs[y]])
+
+    return 1/(1 + sum_of_squares)
+
+# Returns the Pearson correlation coefficient for x and y
+def sim_pearson(prefs, x, y):
+    # get the list of mutually rated items
+    si = {}
+    for item in prefs[x]:
+        if item in prefs[y]:
+            si[item] = 1
+
+    # find the number of elements
+    n = len(s)
+
+    # if no ratings are common to x and y return 0
+    if n==0: return 0
+
+    # Add up all the preferences
+    sum1 = sum([prefs[x][it] for it in si])
+    sum2 = sum([prefs[y][it] for it in si])
+
+    # sum up the squares 
+    sum1_Sq = sum([pow(prefs[x][it], 2) for it in si])
+    sum2_Sq = sum([pow(prefs[y][it], 2) for it in si])
+
+    # Sum up the products
+    p_Sum = sum([prefsp[x][it]*prefs[y][it] for it in si])
+
+    # Calculate the pearson score
+    num = p_Sum-(sum1*sum2/n)
+    den = sqrt((sum1_Sq - pow(sum1, 2) / n) * (sum2_Sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+    
+    r = num/den
+    return r
+
+if __name__ == "__main__":
+    # Running tests here.
+    # Two methods used for determing the similarities of individuals based on there ratings on movies
+    # Eucleadin and Pearson.
+    5
